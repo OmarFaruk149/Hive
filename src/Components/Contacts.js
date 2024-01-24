f6f17648f212e603c091bb375f5945b44ad3949a
OmarFaruk149 <straightlife149@gmail.com>
2024-01-24 20:05:14 +0600

    little

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,55 +1,15 @@
 import React, { useEffect, useState } from "react";
 import ImageLayout from "./ImageLayout";
 import DP from "../images/fb dp.jpg";
+import {UserData} from "./UserDataFor";
 
 export default function Contacts() {
-  const [data, setData] = useState([]);
-
-  const getData = () => {
-    let jsonData = [
-      {
-        user_img: DP,
-        user_name: "Omar Faruk",
-        key: 298,
-      },
-
-      {
-        user_img: "https://pbs.twimg.com/media/F6khzrwXUAAvdxJ.jpg",
-        user_name: "Lufy",
-        key: 298,
-      },
-      {
-        user_img: "https://pbs.twimg.com/media/F6khzrwXUAAvdxJ.jpg",
-        user_name: "Nabil bin khalid",
-        key: 298,
-      },
-      {
-        user_img: "https://pbs.twimg.com/media/F6khzrwXUAAvdxJ.jpg",
-        user_name: "Leo boss",
-        key: 298,
-      },
-      {
-        user_img: "https://pbs.twimg.com/media/F6khzrwXUAAvdxJ.jpg",
-        user_name: "Hashira",
-        key: 298,
-      },
-      {
-        user_img: "https://pbs.twimg.com/media/F6khzrwXUAAvdxJ.jpg",
-        user_name: "Roronoa Zoro",
-        key: 298,
-      },
-    ];
-    setData(jsonData);
-  };
-  useEffect(() => {
-    getData();
-  }, []); 
 
   return (
     <div className="">
       <div className=" text-xl font-semibold py-4">Contacts</div>
       <div className="">
-        {data.map((item) => (
+        {UserData.map((item) => (
           <ImageLayout object={item} />
         ))}
       </div>
--- src/Components/ImageLayout.js
+++ src/Components/ImageLayout.js
@@ -4,10 +4,10 @@ export default function ImageLayout({object}) {
   return (
     <div className="bg-gray-800 p-1 my-2 flex flex-row space-x-2  hover:transform hover:scale-110 transition-transform duration-300 rounded-md">
         <div className="">
-            <img src={object.user_img} alt="" className="w-8 h-8 p-1 rounded-full" key={object.key}/>
+            <img src={object.image} alt="" className="w-8 h-8 p-1 rounded-full" key={object.post_ID}/>
         </div>
         <div className=" text-sm font-sans p-1">
-            {object.user_name}
+            {object.name}
         </div>
     </div>
   )
